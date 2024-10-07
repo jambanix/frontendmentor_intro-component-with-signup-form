e0d7044ae46f33e7537bff8850bde0b4a81441f2
jambanix <jambanix@pm.me>
2024-10-07 19:45:40 +0100

    add all js functionality

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ let inputErrorMapping = {};
 const textInputs = document.querySelectorAll(".text-input");
 const submitBtn = document.querySelector(".btn-submit");
 
+// Mimic success since not posting to a backend
 const mimicSubmit = () => {
-
+    alert("success");
+    textInputs.forEach(input => {
+        setError(input, false);
+        input.value = "";
+        textInputs[0].focus();
+    })
 }
+
+// Set the error message and styling for input if fails validation
+const setError = (input, isError) => {
+    inputErrorMapping[input.getAttribute("name")].forEach(error => {
+        isError ? error.classList.remove("hide") : error.classList.add("hide");
+    });
+    isError ? input.classList.add("input-error") : input.classList.remove("input-error");
+}
+
 
 // Returns object with input name as key and array with the 2 elements for class toggle
 const inputErrorElements = (input) => {
@@ -19,35 +34,60 @@ const inputErrorElements = (input) => {
     ]};
 }
 
-//  Map to the object 
+//  Map to the object and set event listeners for reset upon typing
 textInputs.forEach(input => {
     const mapped = inputErrorElements(input);
     Object.assign(inputErrorMapping, mapped);
+    input.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+
+        }
+        else {
+            setError(input, false);
+        }
+    });
 })
 
-
-const setError = (input) => {
-    inputErrorMapping.input.forEach(error => {
-        error.classList.toggle("hide");
-        input.classList.toggle("error");
-    });
-}
-
-
-
-
+// Style the rows upon error and return true or false if error
 const validateInput = (input) => {
-    const notEmpty = (input) => (input.text.value) ? true: false;
+    const notEmpty = (input) => (input.value) ? true : false;
     const validateEmail = (value) => {
         const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return value.match(regex) ? true : false;
     }
-    if (input.text.value === "email") {
-        notEmpty(input) && validateEmail(input.text.value) ? mimicSubmit() : setError(input);
+
+    // email
+    if (input.value === "email") {
+        if(notEmpty(input) && validateEmail(input.value)) {
+            setError(input, false);
+            return true;
+        }
+        else {
+            setError(input, true);
+            return false;
+        }
     }
     
+    // everything else
+    else {
+        if(notEmpty(input)) {
+            setError(input, false);
+            return true;
+        }
+        else {
+            setError(input, true);
+            return false;
+        }
+    }
 }
 
+// Submit button click event
 submitBtn.addEventListener("click", () => {
-
-})
+    let valid = true;
+    textInputs.forEach(input => {
+        validateInput(input) ? valid = true : valid = false;
+    })
+    if (valid) {
+        mimicSubmit();
+    }
+});
